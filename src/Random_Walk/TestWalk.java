106343464bfcad9f2434964bb0370ccf2287bc4b
Nichlas <Nichlas@192.168.1.184>
2017-02-15 13:39:46 +0100

    Made a for loop in order to rune my 2 objects walk1 and walk2 through it i think it works but i dont know how to change the steps count

---
+++ src/Random_Walk/TestWalk.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Random_Walk;
+import java.util.Scanner;
+/**
+ *
+ * @author Nichlas
+ */
+
+public class TestWalk {
+
+    public static void main(String[] args) {
+        RandomWalk walk1 = new RandomWalk(5, 10);
+
+        int maxSteps;   // maximum number of steps in a walk
+        int maxCoord;   // the maximum x and y coordinate
+        int x, y;       // starting x and y coordinates for a walk
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nRandom Walk Test Program");
+        System.out.println();
+        System.out.print("Enter the boundary for the square: ");
+        maxCoord = scan.nextInt();
+        System.out.print("Enter the maximum number of steps: ");
+        maxSteps = scan.nextInt();
+        System.out.print("Enter the starting x and y coordinates with "
+                + "a space between: ");
+        x = scan.nextInt();
+        y = scan.nextInt();
+
+        RandomWalk walk2 = new RandomWalk(maxCoord, maxSteps, x, y); 
+        walk1.takeStep();
+        walk1.takeStep();
+        
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Steps taken is\n " + walk1);
+            System.out.println("steps taken is\n " + walk2);
+        }
+    }
+
+    public TestWalk() {
+
+    }
+
+}
+
