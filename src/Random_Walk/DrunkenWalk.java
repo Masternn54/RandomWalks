0e22adc197a8bd97f82815e4594ea963b017851d
Nichlas <Nichlas@192.168.1.184>
2017-02-18 19:07:47 +0100

    DrunkenWalk update to make it work added a for loop properly to work

--- src/Random_Walk/DrunkenWalk.java
+++ src/Random_Walk/DrunkenWalk.java
@@ -5,120 +5,37 @@
  */
 package Random_Walk;
 
-import java.util.Random;
-
 /**
  *
  * @author Nichlas
  */
 public class DrunkenWalk {
-
-    private static int xstart;
-    private static int ystart;
-    private static int max;
-    private static int steps;
-    private static int edge;
     private static int outofBounds;
 
     public static void main(String[] args) {
-
-        for (int i = 0; i < 100; i++) {
-            DrunkenWalk Walk = new DrunkenWalk(max, edge);
-
-            Walk.takeStep();
-
-            System.out.println(Walk.toString());
-        }
-
-        if (inBounds() == true) {
-            System.out.println("Still inside square");
-        } else {
-            outofBounds++;
-            System.out.println("Out of Square");
-        }
-
-     
-    }
-
-    DrunkenWalk(int max, int edge) {
-        DrunkenWalk.max = max;
-        DrunkenWalk.edge = edge;
-        xstart = 0;
-        ystart = 0;
-        steps = 0;
-        outofBounds = 0;
-    }
-
-    DrunkenWalk(int max, int edge, int xstart, int ystart, int outofBounds) {
-        DrunkenWalk.max = max;
-        DrunkenWalk.edge = edge;
-        DrunkenWalk.xstart = xstart;
-        DrunkenWalk.ystart = ystart;
-        DrunkenWalk.outofBounds = outofBounds;
-    }
-    @Override
-     public String toString() {
-        return "steps: " + steps + "\n" + "position: " + "(" + xstart + "," + ystart + ")";
-
-    }
-
-    public void takeStep() {
-        // up = 1 down = 2 left = 3 right = 4  
-        Random rand = new Random();
-
-        int n = rand.nextInt(4) + 1;
-
-        switch (n) {
-            case 1://right
-                xstart++;
-                System.out.println(steps++);
-                System.out.println("you Stumbled Right");
-                break;
-
-            case 2://left
-                xstart--;
-                System.out.println(steps++);
-                System.out.println("you Stumbled Left");
-                break;
-            case 3://up
-                ystart++;
-                System.out.println(steps++);
-                System.out.println("you Stumbled Forwards");
-                break;
-            case 4://down
-                ystart--;
-                System.out.println(steps++);
-                System.out.println("you Stumbled Backwards");
-                break;
-
-        }
-    }
-
-    public boolean moreSteps() {
-        if (steps < max) {
-            return true;
-        } else {
-            return false;
-        }
-
-    }
-
-    /**
-     *
-     * @return
-     */
-    public static boolean inBounds() {
-        if (xstart + ystart < edge) {
-            return true;
-        } else {
+        
+         for (int i = 0; i < 100; i++)
+        {
+            RandomWalk DrunkWalk = new RandomWalk(200, 50);
             
-            return false;
-
+            DrunkWalk.walk();
+            
+            System.out.println(DrunkWalk.toString());
+            
+            
+             if (DrunkWalk.inBounds() == false)
+            {
+                outofBounds++;
+                System.out.println("You went outside the square");
+            }
+            
         }
         
-
+        System.out.println("the number of times he fell off: " + outofBounds);
+        
     }
-   
-     
     
 }
+    
+
+
