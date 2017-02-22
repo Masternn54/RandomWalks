e09add900e114504844adaf4a3dc4b06d7f81652
Nichlas <Nichlas@192.168.1.184>
2017-02-22 12:46:32 +0100

    Collisions finished task 10 in collisions

--- src/Random_Walk/Collisions.java
+++ src/Random_Walk/Collisions.java
@@ -14,30 +14,38 @@ public class Collisions {
     private int largeBoundary = 2000000;
     private static int largeMaxSteps = 10000;
     private static int counter = 0;
-
+    
     public static void main(String[] args) {
         RandomWalk particle1 = new RandomWalk(-3, 0);
         RandomWalk particle2 = new RandomWalk(3, 0);
-
+        samePosition(particle1, particle2); 
+        
         for (int i = 0; i < 100; i++) {
 
             particle1.takeStep();
             particle2.takeStep();
-
-            // System.out.println(particle1.toString());
-            // System.out.println(particle2.toString());
+            boolean isCollided = samePosition(particle1, particle2);
+            if(isCollided) 
+            System.out.println("they have collided");
             
-            // it does return the number of collisions   
+              
+             System.out.println(particle1.toString());
+             System.out.println(particle2.toString());
+            
+          //   it does return the number of collisions   
             if ((particle1.getX() == particle2.getX()) && (particle1.getY() == particle2.getY())) {
-                System.out.println("number of collisions " + counter++);
+                System.out.println("number of collisions " + ++counter);
 
-            }
-
+           }
+            
+            
         }
 
     }
     public static boolean samePosition (RandomWalk particle1, RandomWalk particle2){
+
         if((particle1.getX() == particle2.getX()) && (particle1.getY() == particle2.getY())){
+          
             return true;}
         else return false;
         
